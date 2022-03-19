e437aa46d825a91c0e207cc479bae761906adc62
Carlos Cuxin <carloscuxin21@gmail.com>
2022-03-18 19:12:21 -0600

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@ for ext_host in "external-api.dev"
 do
   HOST_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
   echo "${HOST_IP} ${ext_host}" | tee -a /etc/hosts
-  npm run dev
+
+  if [ ! -d "node_modules/" ]; then
+    exec sh -c "npm install"
+  fi
+
+  exec sh -c "npm run dev"
 done
-exec sh -c "tail -f /dev/null;"
